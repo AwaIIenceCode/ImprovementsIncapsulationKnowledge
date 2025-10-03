e527abb5f8a1a17a695bf715ff726787db05f53a
AwallenceCode <awallencecode@gmail.com>
2025-10-03 22:31:06 +0300

    create file

--- ImprovementsIncapsulationKnowledge/Program.cs
+++ ImprovementsIncapsulationKnowledge/Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 
+class Pet
+{
+    private string _name;
+    private byte _age;
+    private string _type;
+    private bool _isFed;
+
+
+}
+
 class Program
 {
     static void Main()
